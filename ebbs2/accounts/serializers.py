b2a56259bb61c964a9dec716d4a960dfad43893c
Erin Winking <erin@douglasavenue.com>
2021-09-11 17:49:02 -0700

    One file was not saved.

--- ebbs2/accounts/serializers.py
+++ ebbs2/accounts/serializers.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
 # USER SERIALIZER
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'email')
 
 # REGISTER SERIALIZER
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -16,11 +20,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(
+            validated_data['username'], validated_data['email'], validated_data['password'])
 
         return user
 
 # LOGIN SERIALIZER
+
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
