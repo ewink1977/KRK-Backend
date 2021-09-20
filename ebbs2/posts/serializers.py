7be0db105040c7bdb8407188865db1c8f3b1f7b5
Erin Winking <erin@douglasavenue.com>
2021-09-20 02:50:45 -0700

    Adjusted POST serializers to properly pull author information!

--- ebbs2/posts/serializers.py
+++ ebbs2/posts/serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 from .models import Post, PostReply
+from django.contrib.auth.models import User
+
+# Post Author Serializer
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        depth = 1
+        fields = ('id', 'username', 'first_name', 'last_name', 'userProfile' )
+
 
 # POST SERIALIZERS
 class PostSerializer(serializers.ModelSerializer):
+    likes = serializers.StringRelatedField(many=True)
+    author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ('id', 'author', 'likes', 'content', 'priority', 'department', 'is_reply', 'created_at' )
 
 class PostReplySerializer(serializers.ModelSerializer):
+    likes = serializers.StringRelatedField(many=True)
+    author = AuthorSerializer(read_only=True)
     class Meta:
         model = PostReply
-        fields = '__all__'
+        fields = ('id', 'author', 'likes', 'content', 'priority', 'department', 'is_reply', 'created_at' )
